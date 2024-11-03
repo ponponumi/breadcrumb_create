d5c62b357e7c490e365fde86fd209cacd2d11ee0
ponponumi <ponponumi@gmail.com>
2024-11-03 13:10:08 +0900

    コンストラクタを追加

--- src/Create.php
+++ src/Create.php
@@ -4,5 +4,22 @@ namespace Ponponumi\BreadcrumbCreate;
 
 class Create
 {
-    //
+    private string $listTag = "ul";
+    private string $itemTag = "li";
+    private $spanMode = false;
+
+    public function __construct(string $tagMode = "ul", $spanMode = false)
+    {
+        switch($tagMode){
+            case "ol":
+                $this->listTag = "ol";
+                break;
+            case "div":
+                $this->listTag = "div";
+                $this->itemTag = "div";
+                break;
+        }
+
+        $this->spanMode = $spanMode;
+    }
 }
